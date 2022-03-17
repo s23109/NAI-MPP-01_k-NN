59aec4249e68c6d2b53280703e96a395dfe43dc8
D K <s23109@pjwstk.edu.pl>
2022-03-17 19:41:02 +0100

    Dodano wstępny odczyt z plików

---
+++ Program/src/Element.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Element {
+
+
+String name_of_object;
+List<Double> coordinates;
+
+
+
+
+}
--- Program/src/Main.java
+++ Program/src/Main.java
@@ -1,7 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+
+
+        System.out.println("Start programu");
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("Dane+Polecenie/iris.data.txt"));
+            String line = null;
+            while ((line = bufferedReader.readLine())!=null){
+                //System.out.println(line);
+                //Wstępna ,,obróbka danych'' ;)
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
